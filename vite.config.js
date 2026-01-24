e7099fc444f13558f10021f0ca586055112dd30a
felipegabrielsc <felipegabriel.sgobicuriel@gmail.com>
2026-01-24 04:36:12 -0300

    alterção final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Agenda Manicure',
+        short_name: 'Agenda',
+        description: 'Gerenciamento de Agenda e Financeiro',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Isso faz abrir sem barra de navegador!
+        icons: [
+          {
+            src: 'pwa-192x192.png', // Você precisará criar essa imagem depois
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png', // E essa também
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
